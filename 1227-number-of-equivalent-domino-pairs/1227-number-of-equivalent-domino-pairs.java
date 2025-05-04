dfc2aa8829b12863db88fab61137d43f9c0c3f4d
Vaishnavi <vaishnavipaswan03@gmail.com>
2025-05-04 19:20:28 +0530

    Time: 2 ms (98.22%), Space: 53.2 MB (41.73%) - LeetHub

---
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] map=new int[100];
+        for(int[] d: dominoes){
+            if(d[0]>d[1]) map[d[0]*10+d[1]]++;
+            else map[d[1]*10+d[0]]++;
+        }
+        int count=0;
+        for(int f:map) count+=(f-1)*f/2;
+        return count;
+    }
+}
