57b649be25d3f74a475f45be51d9953b57df3cdf
Vaishnavi <vaishnavipaswan03@gmail.com>
2025-06-16 00:34:49 +0530

    Time: 0 ms (100%), Space: 42.4 MB (19.93%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,26 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left=0;
-        int right=nums.length-1;
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[mid]>=nums[left]){
-                if(nums[left]<=target && target<=nums[mid]){
-                    right=mid-1;
-                }else{
-                    left=mid+1;
-                }
-            }else{
-                if(nums[mid]<=target && target<=nums[right]){
-                    left=mid+1;
-                }else{
-                    right=mid-1;
-                }
-            }
+        int p=pivot(nums);
+        if(p==-1) return binarySearch(nums,target,0,nums.length-1);
+        if(target==nums[p]) return p;
+        if(target>=nums[0]) return binarySearch(nums,target,0,p);
+        else return binarySearch(nums,target,p+1,nums.length-1);
+    }
+    public static int binarySearch(int nums[],int target,int start,int end){
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) start=mid+1;
+            else end=mid-1;
+        }
+        return -1;
+    }
+    public static int pivot(int nums[]){
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+             if(mid<end && nums[mid]>nums[mid+1]) return mid;
+             if(mid>start && nums[mid]<nums[mid-1]) return mid-1;
+             if(nums[mid]<=nums[start]) end= mid-1;
+            else start=mid+1;
         }
         return -1;
     }
 }
-        
