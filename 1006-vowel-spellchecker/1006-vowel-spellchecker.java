a13172796f89889a3d7b140aab62f144a2269809
Vaishnavi <vaishnavipaswan03@gmail.com>
2025-09-14 10:01:50 +0530

    Time: 20 ms (84.83%), Space: 48.7 MB (54.98%) - LeetHub

---
+++ 1006-vowel-spellchecker/1006-vowel-spellchecker.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String[] spellchecker(String[] wordlist, String[] queries) {
+        Set<String> exact=new HashSet<>(Arrays.asList(wordlist));
+        Map<String,String> lowerMap=new HashMap<>();
+        Map<String,String> vowelMap=new HashMap<>();
+        for(String w: wordlist){
+            String wl=w.toLowerCase();
+            lowerMap.putIfAbsent(wl,w);
+            String masked=maskVowels(wl);
+            vowelMap.putIfAbsent(masked,w);
+        }
+        String ans[]=new String[queries.length];
+        for(int i=0;i<queries.length;i++){
+            String q=queries[i];
+            if(exact.contains(q)){
+                ans[i]=q;
+                continue;
+            }
+        String ql=q.toLowerCase();
+        if(lowerMap.containsKey(ql)){
+            ans[i]=lowerMap.get(ql);
+            continue;
+        }
+        String qMasked=maskVowels(ql);
+        if(vowelMap.containsKey(qMasked)){
+            ans[i]=vowelMap.get(qMasked);
+        }
+        else{
+            ans[i]="";
+        }
+      }
+      return ans;
+    }
+    public boolean isVowel(char c){
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+    }
+    public String maskVowels(String s){
+        char arr[]=s.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            if(isVowel(arr[i])) arr[i]='#';
+        }
+        return new String(arr);
+    }
+}
